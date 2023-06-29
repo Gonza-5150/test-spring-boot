b823d3d524d539cdd11d311ff29456122aa9d159
Gonza-5150 <estevesgonzalo@hotmail.com>
2023-06-29 19:05:13 -0300

    courses route expose

---
+++ src/main/java/com/test/testspringboot/Controller.java
@@ -0,0 +1,20 @@
+package com.test.testspringboot;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class Controller {
+    // Course: id, name, author
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1, "Learn Docker", "Gonza"),
+                new Course(2, "Learn Devops", "Gonza")
+        );
+    }
+}
